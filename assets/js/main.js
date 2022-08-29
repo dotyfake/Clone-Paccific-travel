a1f1be454c54cc5373e0a369abb8760cf674857e
Doty <douthanh97@gmail.com>
2022-08-29 17:23:54 +0700

    test commit git

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function openNavbar() {
+var navbar = document.querySelector('.navbar-list');
+
+if (navbar.style.display == 'block'){
+    navbar.style.display = 'none'
+} else {
+    navbar.style.display = 'block'
+}
+    
+}
